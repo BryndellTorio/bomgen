d1f9412218a34651357851a4aed113763ea8f435
Engr. Bryndell Torio <bryndell.torio@global-imi.com>
2023-01-22 12:28:41 +0800

    Added message box and auto generates error log file to list all found errors.

--- checker.py
+++ checker.py
@@ -1,21 +1,18 @@
-def compare_list(List1,List2):
-    comparison_results = []
+def compare_list(List1, List2):
+    comparison_results=[]
     for i in range(len(List1)):
         for j in range(len(List2)):
             if (List1[i] == List2[j]):
                 comparison_results.append(List1[i])
     return comparison_results
 
-# sample_Data = ['C1','C2','C3']
-# sample_quantity = 2
-
-def compare_quantity_to_reference(Reference_List,Quantity_List):
-    compare_QTR_results = []
+def compare_quantity_to_reference(Reference_List, Quantity_List):
+    QTR_Results=[]
     t_Ref_List = str(Reference_List).split(',')
     length_Ref_List = len(t_Ref_List)
 
     if (length_Ref_List != Quantity_List):
-        compare_QTR_results.append(f'refLength={length_Ref_List}:quantity={Quantity_List}')
-        return compare_QTR_results
+        QTR_Results.append(f'refCnt={length_Ref_List}:qtyCnt={Quantity_List}')
+        return QTR_Results
     else:
         return False
--- main.py
+++ main.py
@@ -1,60 +1,107 @@
+# Core modules
+import os
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
+# 3rd party modules
+from openpyxl import load_workbook, Workbook
+
 # Locally defined modules
 import checker
 import setup
-import tkinter as tk
-
-# Python modules
-from openpyxl import load_workbook, Workbook
-
-# Path to BOM Data.
-path_to_data_1 = "C:\\Users\\bryndell.torio\\OneDrive - Integrated Micro-Electronics Inc\\Design\\Training\\bomgen\\Data\\bills_of_materials.xlsx"
-path_to_data_2 = "C:\\Users\\bryndell.torio\\OneDrive - Integrated Micro-Electronics Inc\\Design\\Training\\bomgen\\Data\\test_data.xlsx"
-path_to_data_3 = "C:\\Users\\bryndell.torio\\OneDrive - Integrated Micro-Electronics Inc\\Design\\Training\\bomgen\\Data\\bills_of_materials-tmp.xlsx"
-
-wb = load_workbook(filename = path_to_data_3)
-ws = wb.active
-
-workbook_max_row = ws.max_row
-workbook_max_column = ws.max_column
-
-check_results = []
-
-for i in range(workbook_max_row-setup.BOM_INDEX + 1):
-    reference_List = ws.cell(row=setup.BOM_INDEX+i,column=3).value
-    quantity_List = ws.cell(row=setup.BOM_INDEX+i,column=2).value
-    comp_QR_result = checker.compare_quantity_to_reference(reference_List,quantity_List)
-
-    if (comp_QR_result != False):
-        check_results.append(str(comp_QR_result))
-
-class myGUI():
-
-    def __init__(self) -> None:
-
-        self.root = tk.Tk()
-
-        self.root.geometry("500x500")
-        self.root.title("Integrater Micro-Electronics Inc. - Design and Development")
 
 
-        self.label = tk.Label(self.root, text="BOM Generator", font=('Arial', 20))
-        self.label.pack(padx=22, pady=22)
+def validate_bom():
+    global t_results
+    global ws
+    window.path_to_data = filedialog.askopenfilename(
+            initialdir="C:\\Users\\bryndell.torio\\OneDrive - Integrated Micro-Electronics Inc\\Design\\Training\\bomgen\\Data", # Change this to $HOME location.
+            title="Select a file",
+            filetypes=(("Excel files", "*.xlsx"),
+                       ("BOM files", "*.BOM"),
+                       ("all files", "*.*")))
 
+    # Loading the selected bom file into the design.
+    wb = load_workbook(filename = window.path_to_data)
+    ws = wb.active
 
-        self.button1 = tk.Button(self.root, text="Generate BOM", font=('Arial', 11), command=self.print_Check)
-        self.button1.pack(padx=10, pady=10)
+    workbook_max_row = ws.max_row
+    workbook_max_column = ws.max_column
 
-        self.button2 = tk.Button(self.root, text="Compare Netlist", font=('Arial', 11))
-        self.button2.pack(padx=10, pady=10)
+    t_results=[]
+    for i in range(workbook_max_row-setup.BOM_INDEX+1):
+        reference_List = ws.cell(row=setup.BOM_INDEX+i,column=3).value
+        quantity_List = ws.cell(row=setup.BOM_INDEX+i,column=2).value
+        comp_QR_result = checker.compare_quantity_to_reference(reference_List,quantity_List)
+        if comp_QR_result:
+            t_results.append(comp_QR_result)
 
-        self.textbox = tk.Text(self.root, height=3, font=('Arial', 22))
-        self.textbox.pack(padx=50,  expand=1)
+    for x in range(len(t_results)):
+        print(str(t_results[x]).split(':'))
 
-        self.root.mainloop()
+    if t_results:
+        error_log()
 
-    def print_Check(self):
-        print(check_results)
+    # error_results=[]
+    # error_results.append(str(ws['A2'].value).split('          '))
+    # placeholder = str(error_results).split(',')
 
-myGUI()
+def error_log():
+    fileName = 'errorlog.txt'
+    errorFile = open(fileName, 'w')
+    errorFile.write(str(t_results))
+    errorFile.close()
 
+    messagebox.showerror(title='Error:',message='Found mismatch in Quantity and Reference.')
 
+    if messagebox.askyesnocancel(title='Debug log',message='Do you like to open debug log?'):
+        os.startfile(fileName)
+
+def temporary_function():
+    tempo_results = str(t_results).split(',')
+    print(tempo_results)
+
+def print_path():
+    print(window.path_to_data)
+
+def print_results():
+    print(t_results)
+
+# Start of GUI loop.
+window = tk.Tk()
+
+window.geometry("500x500")
+window.title("Integrater Micro-Electronics Inc. - Design and Development")
+icon = tk.PhotoImage(file='.\\Images\\imi-logo.png')
+window.iconphoto(True,icon)
+
+label = tk.Label(window,
+                 text="BOM Generator",
+                 font=('Arial', 20))
+label.pack(padx=22, pady=22)
+
+button1 = tk.Button(window,
+                    text="Generate BOM",
+                    font=('Arial', 11),
+                    command=validate_bom)
+button1.pack(padx=10, pady=10)
+
+button2 = tk.Button(window,
+                    text="Path",
+                    font=('Arial', 11),
+                    command=print_path)
+button2.pack(padx=10, pady=10)
+
+button3 = tk.Button(window,
+                    text="Check",
+                    font=('Arial', 11),
+                    command=print_results)
+button3.pack(padx=10, pady=10)
+
+textbox = tk.Text(window,
+                  height=3,
+                  font=('Arial', 22))
+textbox.pack(padx=50,  expand=1)
+
+window.mainloop()
+# End of GUI loop.
